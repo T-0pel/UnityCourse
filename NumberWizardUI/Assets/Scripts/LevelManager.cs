69e644ef9ba2e618084f3da7837befe82de48e22
Tomas Oplatek <oplatek.t@gmail.com>
2017-10-13 18:02:58 +0100

    UI finished

---
+++ NumberWizardUI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for:" + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void ReturnToStart()
+    {
+        Debug.Log("Returning to start screen");
+        SceneManager.LoadScene("Start");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game quitting");
+        Application.Quit();
+    }
+}
---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    private int Max { get; set; }
+    private int Min { get; set; }
+    private int Guess { get; set; }
+    public int MaxGuessesAllowed = 5;
+    public Text NumberText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        Max = 1000;
+        Min = 1;
+        Max++;
+        Guess = Random.Range(Min, Max);
+        NumberText.text = Guess.ToString();
+    }
+
+    public void GuessHigher()
+    {
+        Min = Guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        Max = Guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        Guess = Random.Range(Min, Max);
+        NumberText.text = Guess.ToString();
+        MaxGuessesAllowed--;
+        if (MaxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+    public void GuessCorrect()
+    {
+        SceneManager.LoadScene("Lose");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
