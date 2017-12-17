262619b510a7f3bd2cf4a28b71a365341e512e3b
Tomas Oplatek <oplatek.t@gmail.com>
2017-12-17 21:53:23 +0000

    Laser Defender

---
+++ Laser Defender/Assets/Entities/Enemy/EnemySpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public float Width;
+    public float Height;
+    public bool MovingRight;
+    public float Padding;
+    public float Speed;
+
+    private float XMin;
+    private float XMax;
+
+    private void Start()
+    {
+        var distance = transform.position.z - Camera.main.transform.position.z;
+        var leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        var rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        XMin = leftmost.x + Padding;
+        XMax = rightmost.x - Padding;
+
+        foreach (Transform child in transform)
+        {
+            var enemy = Instantiate(EnemyPrefab, child.transform.position, Quaternion.identity);
+            enemy.transform.parent = child;
+        }
+    }
+
+    private void Update()
+    {
+        if (MovingRight)
+        {
+            if (transform.position.x + Width / 2 >= XMax)
+            {
+                MovingRight = false;
+            }
+            else
+            {
+                transform.position += Vector3.right * Speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (transform.position.x - Width / 2 <= XMin)
+            {
+                MovingRight = true;
+            }
+            else
+            {
+                transform.position += Vector3.left * Speed * Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height));
+    }
+}
---
+++ Laser Defender/Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float Padding;
+
+    private float XMin;
+    private float XMax;
+    private Rigidbody2D Rigidbody;
+
+    private void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+        var distance = transform.position.z - Camera.main.transform.position.z;
+        var leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        var rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        XMin = leftmost.x + Padding;
+        XMax = rightmost.x - Padding;
+    }
+
+    private void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var speedVector = new Vector3(horizontalInput * Speed, 0, 0);
+        Rigidbody.velocity = speedVector;
+
+        transform.position = new Vector3(Mathf.Clamp(Rigidbody.position.x, XMin, XMax), Rigidbody.position.y, 0);
+    }
+}
